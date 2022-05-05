eb30584090017af7036eedc2c38d63d0b1e7feb9
Jacky Fan <mrf1013@outlook.com>
2022-05-05 21:59:02 +0800

    add Dance animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Dance from "./Dance";
+
 function App() {
   return (
     <div className="App">
       <h2>Hello World</h2>
+      <Canvas
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        style={{
+          backgroundColor: '#111a21',
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.4} />
+        <Suspense fallback={null}>
+          <Dance position={[0.025, -0.9, 0]} />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
