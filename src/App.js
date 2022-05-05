c1422ccc6356ee0ff037d09bd030a65aae8bb498
Jacky Fan <mrf1013@outlook.com>
2022-05-05 23:05:53 +0800

    add FadeIn animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DanceCanvas from './components/DanceCanvas';
 import { SocialIcon } from 'react-social-icons';
+import FadeIn from 'react-fade-in';
 
 function App() {
   return (
@@ -10,25 +11,27 @@ function App() {
           <DanceCanvas />
         </div>
         <div class="flex justify-center items-center h-screen">
-          <center>
-            <h1 className="text-5xl font-bold font-mono">
-              Hello world!
-            </h1>
-            <p className="text-lg font-mono pt-8">
-              Hello Worlds
-            </p>
-            <span class="inline-grid grid-cols-3 gap-4 py-8">
-              <span>
-                <SocialIcon url="https://github.com/redfrogsss" bgColor='#f3f4f6' />
+          <FadeIn>
+            <center>
+              <h1 className="text-5xl font-bold font-mono">
+                Hello world!
+              </h1>
+              <p className="text-lg font-mono pt-8">
+                Hello Worlds
+              </p>
+              <span class="inline-grid grid-cols-3 gap-4 py-8">
+                <span>
+                  <SocialIcon url="https://github.com/redfrogsss" bgColor='#f3f4f6' />
+                </span>
+                <span>
+                  <SocialIcon url="https://twitter.com/redfrogss" />
+                </span>
+                <span>
+                  <SocialIcon url="mailto:hello@redfrogss.dev" bgColor='#f3f4f6' />
+                </span>
               </span>
-              <span>
-                <SocialIcon url="https://twitter.com/redfrogss" />
-              </span>
-              <span>
-                <SocialIcon url="mailto:hello@redfrogss.dev" bgColor='#f3f4f6' />
-              </span>
-            </span>
-          </center>
+            </center>
+          </FadeIn>
         </div>
       </div>
     </div>
