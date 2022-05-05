bf4a58ca178f40dfdbdb968eba98bbf208abdf15
Jacky Fan <mrf1013@outlook.com>
2022-05-05 22:44:30 +0800

    create social icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import DanceCanvas from './components/DanceCanvas';
+import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
     <div className="App">
-
-      <div class="grid grid-cols-2 gap-4 px-64">
-        <div>
+      <div class="grid grid-cols-2 px-64 content-center">
+        <div class="flex justify-center items-center h-screen">
           <DanceCanvas />
         </div>
-        <div>
-          <p class="py-64">
-            <center>
-              <h1 className="text-5xl font-bold font-mono">
-                Hello world!
-              </h1>
-            </center>
-          </p>
+        <div class="flex justify-center items-center h-screen">
+          <center>
+            <h1 className="text-5xl font-bold font-mono">
+              Hello world!
+            </h1>
+            <span class="inline-grid grid-cols-3 gap-4 py-16">
+              <span>
+                <SocialIcon url="https://github.com/redfrogsss" bgColor='#f3f4f6' />
+              </span>
+              <span>
+                <SocialIcon url="https://twitter.com/redfrogss" />
+              </span>
+              <span>
+                <SocialIcon url="mailto:hello@redfrogss.dev" bgColor='#f3f4f6' />
+              </span>
+            </span>
+          </center>
         </div>
       </div>
     </div>
