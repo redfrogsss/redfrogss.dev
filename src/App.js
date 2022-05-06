dfbb772cbe806791fb564ea94d23fc575ea6dc54
Jacky Fan <mrf1013@outlook.com>
2022-05-06 22:47:52 +0800

    create Projects.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import DanceCanvas from './components/DanceCanvas';
-import { SocialIcon } from 'react-social-icons';
-import FadeIn from 'react-fade-in';
 import Intro from './components/Intro';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Intro />
-      <div class="grid grid-cols-2 px-64 content-center h-[100vh]">
-        <h1 className="text-5xl font-bold font-mono">Projects</h1>
-        <h1 className="text-5xl font-bold font-mono">Projects</h1>
-      </div>
+      <Projects />
     </div >
   );
 }
