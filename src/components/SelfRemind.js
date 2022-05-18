4125351024aa0097e12bf25b8c2bd6d2d5e5066a
Jacky Fan <mrf1013@outlook.com>
2022-05-19 00:32:04 +0800

    add alt to SelfRemind

--- src/components/SelfRemind.js
+++ src/components/SelfRemind.js
@@ -20,7 +20,7 @@ export default function SelfRemind() {
         <>
             <div class="grid grid-cols-1 lg:grid-cols-2 bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
                 <div class="flex lg:hidden">
-                    <img class="object-cover w-[100%]" src={SelfRemindPic}></img>
+                    <img class="object-cover w-[100%]" src={SelfRemindPic} alt="Screenshot of SelfRemind"></img>
                 </div>
                 <div class="pl-2">
                     <h1 className="text-2xl font-bold">SelfRemind</h1>
@@ -43,7 +43,7 @@ export default function SelfRemind() {
                     <button type="button" class="rounded-full bg-blue-600 px-4 py-4 my-4" onClick={()=>{ButtonOnClickHandler("Github")}}>Github</button>
                 </div>
                 <div class="hidden lg:flex">
-                    <img class="object-cover w-[100%]" src={SelfRemindPic}></img>
+                    <img class="object-cover w-[100%]" src={SelfRemindPic} alt="Screenshot of SelfRemind"></img>
                 </div>
             </div>
         </>
