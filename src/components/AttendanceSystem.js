8a03a411f1db7b9707211da94a576f5fd6a004e8
Jacky Fan <mrf1013@outlook.com>
2022-05-19 00:09:29 +0800

    add Responsive Design for SANS

--- src/components/AttendanceSystem.js
+++ src/components/AttendanceSystem.js
@@ -3,12 +3,15 @@ import SansPic from "../img/sans.png"
 export default function AttendanceSystem() {
     return (
         <>
-            <div class="grid grid-cols-2 bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+            <div class="grid grid-cols-1 lg:grid-cols-2 bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+                <div class="flex lg:hidden">
+                    <img class="object-cover w-[100%]" src={SansPic}></img>
+                </div>
                 <div class="pl-2">
                     <h1 className="text-xl font-bold">Students Attendance System</h1>
                     <p>
                         <text className="text-lg pt-4 pr-4">
-                        a Student Attendance Management System built with ExpressJS / Java GlassFish, Bootstrap, JQuery and MySQL.
+                            a Student Attendance Management System built with ExpressJS / Java GlassFish, Bootstrap, JQuery and MySQL.
                             <ul class="list-disc pl-8">
                                 <li>
                                     Easy Attendance Management
@@ -25,7 +28,7 @@ export default function AttendanceSystem() {
                     <button type="button" class="rounded-full bg-blue-600 px-4 py-4 my-4">Github</button>
                     <button type="button" class="rounded-full bg-blue-600 px-4 py-4 my-4">Github</button>
                 </div>
-                <div>
+                <div class="hidden lg:flex">
                     <img class="object-cover w-[100%]" src={SansPic}></img>
                 </div>
             </div>
