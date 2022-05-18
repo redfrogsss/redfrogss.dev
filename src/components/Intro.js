094278113d20f18beef12e6520db61135ba7a302
Jacky Fan <mrf1013@outlook.com>
2022-05-18 23:56:15 +0800

    remove fadeIn effect

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,27 +9,25 @@ export default function Intro() {
                 <DanceCanvas />
             </div>
             <div class="flex justify-center items-center h-screen">
-                <FadeIn>
-                    <center>
-                        <h1 className="text-5xl font-bold font-mono">
-                            Jacky FAN
-                        </h1>
-                        <p className="text-lg font-mono pt-8">
-                            A PolyU student studying Computing.
-                        </p>
-                        <span class="inline-grid grid-cols-3 gap-4 py-8">
-                            <span>
-                                <SocialIcon url="https://github.com/redfrogsss" bgColor='#f3f4f6' />
-                            </span>
-                            <span>
-                                <SocialIcon url="https://twitter.com/redfrogss" />
-                            </span>
-                            <span>
-                                <SocialIcon url="mailto:hello@redfrogss.dev" bgColor='#f3f4f6' />
-                            </span>
+                <center>
+                    <h1 className="text-5xl font-bold font-mono">
+                        Jacky FAN
+                    </h1>
+                    <p className="text-lg font-mono pt-8">
+                        A PolyU student studying Computing.
+                    </p>
+                    <span class="inline-grid grid-cols-3 gap-4 py-8">
+                        <span>
+                            <SocialIcon url="https://github.com/redfrogsss" bgColor='#f3f4f6' />
                         </span>
-                    </center>
-                </FadeIn>
+                        <span>
+                            <SocialIcon url="https://twitter.com/redfrogss" />
+                        </span>
+                        <span>
+                            <SocialIcon url="mailto:hello@redfrogss.dev" bgColor='#f3f4f6' />
+                        </span>
+                    </span>
+                </center>
             </div>
         </div>
     );
