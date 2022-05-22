0631b00441f365b9572d2ee4db70e7f06e7676dc
Jacky Fan <mrf1013@outlook.com>
2022-05-22 09:16:12 +0800

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Intro />
       <Projects />
+      <Footer />
     </div >
   );
 }
