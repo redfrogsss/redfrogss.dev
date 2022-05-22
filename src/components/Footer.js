6d22b84d50e21a8eb595b44b3cca45eddfe5a6ae
Jacky Fan <mrf1013@outlook.com>
2022-05-22 16:24:21 +0800

    change Footer's font

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div>
             <hr class="mt-6 mb-3 border-slate-400 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block mt-3 mb-6 text-lg text-slate-400 text-center">
+            <span class="block mt-3 mb-6 text-md text-slate-400 text-center font-mono">
                <a href="https://redfrogss.dev" class="hover:underline">Jacky FAN</a> @ 2022. Check out the <a href="https://github.com/redfrogsss/redfrogss.dev" class="hover:underline">Dirty Code</a> here!
             </span>
         </div>
