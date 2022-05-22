e72aa0e19cbc20ef6ee00bf2f7003df2b380b1ce
Jacky Fan <mrf1013@outlook.com>
2022-05-22 09:06:51 +0800

    add timeline component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,15 +6,29 @@ export default function Projects() {
     return (
         <div className="px-8 md:px-16 lg:px-32 content-center">
             <h1 className="text-5xl font-bold font-mono py-16">Projects</h1>
-            <div class="pb-4">
-                <SelfRemind />
-            </div>
-            <div class="py-4">
-                <RedStream />
-            </div>
-            <div class="py-4">
-                <AttendanceSystem />
-            </div>
+            <ol class="relative border-l border-gray-200 dark:border-gray-700">
+                <li class="mb-10 ml-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">February 2022</time>
+                    <div class="m-4">
+                        <SelfRemind />
+                    </div>
+                </li>
+                <li class="mb-10 ml-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">March 2022</time>
+                    <div class="m-4">
+                        <RedStream />
+                    </div>
+                </li>
+                <li class="ml-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">April 2022</time>
+                    <div class="m-4">
+                        <AttendanceSystem />
+                    </div>
+                </li>
+            </ol>
         </div>
     );
 
