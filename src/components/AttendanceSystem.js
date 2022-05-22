874d83474f2219017d94b79baf231f80f0d00cba
Jacky Fan <mrf1013@outlook.com>
2022-05-22 09:33:05 +0800

    add progress bar

--- src/components/AttendanceSystem.js
+++ src/components/AttendanceSystem.js
@@ -26,8 +26,17 @@ export default function AttendanceSystem() {
                 <div class="flex lg:hidden">
                     <img class="object-cover w-[100%]" src={SansPic} alt="Screenshot of Student Attendance Management System"></img>
                 </div>
-                <div class="pl-2">
+                <div class="pl-2 px-8">
                     <h1 className="text-2xl font-bold font-mono">Students Attendance System</h1>
+                    <div className="my-2.5">
+                        <div class="flex justify-between mb-1">
+                            <span class="text-base font-medium font-mono text-blue-500 dark:text-white">Development Progress</span>
+                            <span class="text-sm font-medium font-mono text-blue-500 dark:text-white">100%</span>
+                        </div>
+                        <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                            <div class="bg-blue-600 h-2.5 rounded-full w-[100%]"></div>
+                        </div>
+                    </div>
                     <p>
                         <text className="text-lg pt-4 pr-4 font-mono">
                             a Student Attendance Management System built with ExpressJS / Java GlassFish, Bootstrap, JQuery and MySQL.
@@ -55,7 +64,7 @@ export default function AttendanceSystem() {
                         </span>
                     </span>
                 </div>
-                <div class="hidden lg:flex">
+                <div class="hidden lg:flex px-8">
                     <img class="object-cover w-[100%]" src={SansPic} alt="Screenshot of Student Attendance Management System"></img>
                 </div>
             </div>
