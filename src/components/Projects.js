7d2f1aeb0581cfc86440d5865c48e31bd63e58be
Jacky Fan <mrf1013@outlook.com>
2022-05-22 09:40:25 +0800

    change order of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,23 +9,23 @@ export default function Projects() {
             <ol class="relative border-l border-gray-200 dark:border-gray-700">
                 <li class="mb-10 ml-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">February 2022</time>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">January 2022 ~ Now</time>
                     <div class="m-4">
                         <SelfRemind />
                     </div>
                 </li>
-                <li class="mb-10 ml-4">
-                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">March 2022</time>
-                    <div class="m-4">
-                        <RedStream />
-                    </div>
-                </li>
                 <li class="ml-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">April 2022</time>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">April ~ May 2021</time>
                     <div class="m-4">
                         <AttendanceSystem />
+                    </div>
+                </li>
+                <li class="mb-10 ml-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">January 2021 ~ April 2021</time>
+                    <div class="m-4">
+                        <RedStream />
                     </div>
                 </li>
             </ol>
