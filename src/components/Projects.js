55d4eaa027309d74052952682cc33d39ced0d4af
Jacky Fan <mrf1013@outlook.com>
2022-05-28 01:13:05 +0800

    add motion to Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import RedStream from "./RedStream";
 import SelfRemind from "./SelfRemind";
 import AttendanceSystem from "./AttendanceSystem";
+import { motion } from "framer-motion"
 
 export default function Projects() {
     return (
@@ -11,21 +12,39 @@ export default function Projects() {
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">January 2022 ~ Now</time>
                     <div class="m-4">
-                        <SelfRemind />
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            viewport={{ once: true }}
+                        >
+                            <SelfRemind />
+                        </motion.div>
                     </div>
                 </li>
                 <li class="ml-2.5 md:ml-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">April ~ May 2021</time>
                     <div class="m-4">
-                        <AttendanceSystem />
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            viewport={{ once: true }}
+                        >
+                            <AttendanceSystem />
+                        </motion.div>
                     </div>
                 </li>
                 <li class="mb-10 ml-2.5 md:ml-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time class="mb-1 text-lg font-mono leading-none text-gray-400 dark:text-gray-500">January 2021 ~ April 2021</time>
                     <div class="m-4">
-                        <RedStream />
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            viewport={{ once: true }}
+                        >
+                            <RedStream />
+                        </motion.div>
                     </div>
                 </li>
             </ol>
