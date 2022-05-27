e16e457cdfb3f6e1efabc425c6df7741b7d14abb
Jacky Fan <mrf1013@outlook.com>
2022-05-28 01:25:13 +0800

    Add loading text in DanceCanvas

--- src/components/DanceCanvas.js
+++ src/components/DanceCanvas.js
@@ -6,21 +6,23 @@ import Dance from "./Dance";
 export default function DanceCanvas() {
 
     return (
-        <Canvas
-            camera={{ position: [2, 0, 12.25], fov: 15 }}
-            style={{
-                backgroundColor: '#0f172a',
-                width: '100%',
-                height: '100vh',
-            }}
-        >
-            <ambientLight intensity={1.25} />
-            <ambientLight intensity={0.1} />
-            <directionalLight intensity={0.4} />
-            <Suspense fallback={null}>
-                <Dance position={[0.025, -0.9, 0]} />
-            </Suspense>
-            {/* <OrbitControls /> */}
-        </Canvas>
+        <Suspense fallback={<span class="text-white">loading...</span>}>
+            <Canvas
+                camera={{ position: [2, 0, 12.25], fov: 15 }}
+                style={{
+                    backgroundColor: '#0f172a',
+                    width: '100%',
+                    height: '100vh',
+                }}
+            >
+                <ambientLight intensity={1.25} />
+                <ambientLight intensity={0.1} />
+                <directionalLight intensity={0.4} />
+                <Suspense fallback={null}>
+                    <Dance position={[0.025, -0.9, 0]} />
+                </Suspense>
+                {/* <OrbitControls /> */}
+            </Canvas>
+        </Suspense>
     );
 }
