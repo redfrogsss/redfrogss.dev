753684c88761ee841f79304420973d48b3798a7c
Jacky Fan <mrf1013@outlook.com>
2022-06-26 02:21:14 +0800

    add comments to App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import Loading from './components/Loading';
 
 function App() {
 
+  // State to determine whether to show the loading screen
   const [showLoading, setShowLoading] = useState(true);
 
+  // Loading screen's delay in ms
   const loadingDelayMs = 5000;
 
   useEffect(() => {
+    // Hide loading screen after the delay time
     setTimeout(() => {
       setShowLoading(false);
     }, loadingDelayMs);
