6ab428131ed07dd4dfeeab552a3e0c5b1570ccb8
Jacky Fan <jackyfan@consumer.org.hk>
2022-06-26 02:05:56 +0800

    add loading indictor for loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 
-const Loading = ({delayMs = 5000}) => {
+const Loading = ({ delayMs = 5000 }) => {
 
     const variants = {
         hidden: { opacity: 0 },
         visible: { opacity: 1 },
     }
 
-    const delay = delayMs / 1000;
+    const delay = (delayMs / 1000) - 1;
 
     return (
         <div className="fixed w-screen h-screen flex justify-center items-center bg-gray-900">
@@ -17,7 +17,12 @@ const Loading = ({delayMs = 5000}) => {
                 variants={variants}
                 transition={{ delay: delay }}
             >
-                <h2>Hello World</h2>
+                <div class="flex items-center justify-center space-x-2 animate-pulse">
+                    <div class="w-4 h-4 bg-blue-400 rounded-full"></div>
+                    <div class="w-4 h-4 bg-blue-400 rounded-full"></div>
+                    <div class="w-4 h-4 bg-blue-400 rounded-full"></div>
+                </div>
+                <div className="mt-2 animate-pulse">Loading...</div>
             </motion.div>
         </div>
     )
